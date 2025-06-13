712471fe0f17fc9ee3907695115e6c9495e68e59
Mikhail Frolov <miha.frolow2017@yandex.ru>
2025-06-13 16:09:27 +0300

    Filterset

--- getter/getter/settings.py
+++ getter/getter/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework',
     'corsheaders',
+    'django_filters',
     'main',
     'users',
     'django_extensions',
@@ -65,6 +66,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
     ],
 }
 SIMPLE_JWT = {
